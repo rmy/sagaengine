564d21baf4b1b0e69aabfdc128cb0f10d0635f67
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2006-07-05 16:08:43 +0000

    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@7 65b84acb-c606-4e19-8676-f724b2686c9c

--- samples/simple/projects/qmake/bootstrap.sh
+++ samples/simple/projects/qmake/bootstrap.sh
@@ -6,6 +6,5 @@ sh ../../../../tools/qmake/strap_app.sh ../../../../add_on/ogre ogre
 sh ../../../../tools/qmake/strap_app.sh ../../ogre_ui ogre_ui
 sh ../../../../tools/qmake/strap_app.sh ../../game game
 
-rm core.pro client.pro ogre.pro ogre_ui.pro game.pro
-
 qmake -o makefile all.pro
+
