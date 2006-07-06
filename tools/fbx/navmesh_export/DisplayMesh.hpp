98debe4799bcb40556175c87c9a99bb527ff4145
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2006-07-06 09:07:38 +0000

    Compile script amd64 Ported to FBX2005.12
    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@9 65b84acb-c606-4e19-8676-f724b2686c9c

--- tools/fbx/navmesh_export/DisplayMesh.hpp
+++ tools/fbx/navmesh_export/DisplayMesh.hpp
@@ -1,34 +1,13 @@
-/*
-SagaEngine library
-Copyright (c) 2002-2006 Skalden Studio AS
-
-This software is provided 'as-is', without any express or implied 
-warranty. In no event will the authors be held liable for any 
-damages arising from the use of this software.
-
-Permission is granted to distribute the library under the terms of the 
-Q Public License version 1.0. Be sure to read and understand the license
-before using the library. It should be included here, or you may read it
-at http://www.trolltech.com/products/qt/licenses/licensing/qpl
-
-The original version of this library can be located at:
-http://www.sagaengine.com/
-
-Rune Myrland
-rune@skalden.com
-*/
-
-
 #ifndef _DISPLAY_MESH_H
 #define _DISPLAY_MESH_H
 
-#include <kfbxplugins/kfbxnamespacedef.h>
+#include <fbxfilesdk_def.h>
 
 // Forward declaration.
-class FBXSDK_NAMESPACE::KFbxNode;
+class FBXFILESDK_NAMESPACE::KFbxNode;
 
 
-void DisplayMesh(FBXSDK_NAMESPACE::KFbxNode* pNode);
+void DisplayMesh(FBXFILESDK_NAMESPACE::KFbxNode* pNode);
 
 
 #endif // #ifndef _DISPLAY_MESH_H
