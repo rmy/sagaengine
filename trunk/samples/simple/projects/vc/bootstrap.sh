b9b4d33a4982db962866b7d979fc5dbb04770e5e
rmy <rmy@99c62bc1-9513-0410-a77b-a34f313f9740>
2006-08-10 08:06:35 +0000

    Trying to set up compiling for vc++.

--- trunk/samples/simple/projects/vc/bootstrap.sh
+++ trunk/samples/simple/projects/vc/bootstrap.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-cd /home/rune/svn/sagaengine/core/projects/vc && sh bootstrap.sh
-cd /home/rune/svn/sagaengine/add_on/client/projects/vc && sh bootstrap.sh
-cd /home/rune/svn/sagaengine/add_on/angelscript/projects/vc && sh bootstrap.sh
-cd /home/rune/svn/sagaengine/add_on/ogre/projects/vc && sh bootstrap.sh
-cd /home/rune/svn/sagaengine/samples/simple/ogre_ui/projects/vc && sh bootstrap.sh
-cd /home/rune/svn/sagaengine/samples/simple/game/projects/vc && sh bootstrap.sh
+python ../../../../tools/python/createVCProjFile.py simple ../../../../env/vc/application > simple.vcproj
