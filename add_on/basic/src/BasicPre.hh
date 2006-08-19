959e9eaf0cf52affcd2ca7c31625cd5807fdf99c
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2006-08-19 10:01:05 +0000

    Some often used code put in basic addon module.
    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@123 65b84acb-c606-4e19-8676-f724b2686c9c

---
+++ add_on/basic/src/BasicPre.hh
@@ -0,0 +1,8 @@
+#ifndef BasicPre_hh
+#define BasicPre_hh
+
+#include <se_core.hpp>
+#include <se_client.hpp>
+
+#endif
+
---
+++ add_on/basic/src/thing/SimplePlayerFactory.cpp
@@ -0,0 +1,37 @@
+#include "SimplePlayerFactory.hpp"
+
+
+using namespace se_core;
+
+namespace se_basic {
+
+	SimplePlayerFactory
+	::SimplePlayerFactory(String* name)
+		: SimpleActorFactory(name) {
+	}
+
+
+	SimplePlayerFactory
+	::~SimplePlayerFactory() {
+	}
+
+
+	Thing* SimplePlayerFactory
+	::create() const {
+		Player* a = new Player(name_->get());
+
+		a->setPickable(isPickable_);
+		a->setCollideable(isCollideable_);
+		a->setDefaultPhysics(physics_);
+		a->setCollide(collide_);
+		a->nextPos().setRadius(radius_);
+		a->setSpawnPoints(spawnPointCount_, spawnPoints_);
+
+		if(script_) {
+			a->setDefaultScript(script_);
+		}
+
+		return a;
+	}
+
+}
