112d064ac5b1d2f5ea26a5f649fee4a434846e91
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2006-08-20 13:00:39 +0000

    Comments.
    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@138 65b84acb-c606-4e19-8676-f724b2686c9c

--- add_on/ogre/src/o3d/schema/O3dSchema.hpp
+++ add_on/ogre/src/o3d/schema/O3dSchema.hpp
@@ -32,21 +32,50 @@ rune@skalden.com
 #include "../widget/o3d_widget.hpp"
 #include "O3dPre.H"
 
-
+/**
+ * The Ogre add_on module is a gluing layer between SagaEngine and the Ogre 3d enginge (http://www.ogre3d.org/).
+ */
 namespace se_ogre {
+	/**
+	 * Global variables used by the Ogre add-on module.
+	 */
 	namespace O3dSchema {
+		/**
+		 * Manager that maintins bindings between SagaEngine things and their representation in Ogre.
+		 */
 		extern _SeOgreExport se_ogre::ThingMOManager thingMOManager;
-
+		/**
+		 * Container of singly linked lists of ThingMO objects.
+		 * Used by the world manager, and ThingMultiMO classes.
+		 */
+		extern _SeOgreExport ThingMOList thingMOList;
 
 		extern _SeOgreExport Ogre::RaySceneQuery* raySceneQuery;
-		extern _SeOgreExport ThingMOList thingMOList;
+		
+		/**
+		 * The Ogre::Root singleton.
+		 */
 		extern _SeOgreExport Ogre::Root* root;
+		/**
+		 * The Ogre::SceneManager singleton.
+		  */
 		extern _SeOgreExport Ogre::SceneManager* sceneManager;
+		
+		/**
+		 * The manager object that controls what ogre should render.
+		 */
 		extern _SeOgreExport WorldManager* worldManager;
+
+		/**
+		 * Manager for input event listeners.
+		 * Register the class that should recieve mouse and input events here.
+		 */
+		extern _SeOgreExport InputManager& inputManager();
+
+		
 		extern _SeOgreExport Ogre::Camera* playerCamera;
 		extern _SeOgreExport Ogre::RenderWindow* window;
 		extern _SeOgreExport SpeechBubble* speechBubble;
-		extern _SeOgreExport InputManager& inputManager();
 		extern _SeOgreExport Console* console;
 		extern _SeOgreExport RenderEventListeners& renderEventListeners();
 
