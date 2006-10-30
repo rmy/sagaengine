dd2898ed200508336beabe1f9be6e6680a3f78c8
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2006-10-30 15:38:22 +0000

    vcproj
    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@272 65b84acb-c606-4e19-8676-f724b2686c9c

---
+++ env/vc/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cd ../..
+cd samples/simple/projects/vc
+sh bootstrap.sh
+cd ../../../..
+cd samples/fly/projects/vc
+sh bootstrap.sh
+cd ../../../..
+cd samples/navmesh/projects/vc/
+sh bootstrap.sh
