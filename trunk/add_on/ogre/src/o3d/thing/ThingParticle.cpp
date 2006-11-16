72185f4359cb02b2432f3ebbf84e45ed206e9719
rmy <rmy@99c62bc1-9513-0410-a77b-a34f313f9740>
2006-11-16 16:07:05 +0000

    Particle system support.

---
+++ trunk/add_on/ogre/src/o3d/thing/ThingParticle.cpp
@@ -0,0 +1,59 @@
+/*
+SagaEngine library
+Copyright (c) 2002-2006 Skalden Studio AS
+
+This software is provided 'as-is', without any express or implied 
+warranty. In no event will the authors be held liable for any 
+damages arising from the use of this software.
+
+Permission is granted to distribute the library under the terms of the 
+Q Public License version 1.0. Be sure to read and understand the license
+before using the library. It should be included here, or you may read it
+at http://www.trolltech.com/products/qt/licenses/licensing/qpl
+
+The original version of this library can be located at:
+http://www.sagaengine.com/
+
+Rune Myrland
+rune@skalden.com
+*/
+
+
+#include "ThingMOInfo.hpp"
+#include "ThingParticle.hpp"
+#include "../schema/O3dSchema.hpp"
+#include <cstdio>
+#include <cstring>
+
+namespace se_ogre {
+
+	ThingParticle
+	::ThingParticle(se_core::PosNode& thing, const ThingMOInfo& info, const ThingMOFactory& factory)
+		: ThingMO(thing, info, factory) {
+
+		// Create a unique entity name
+		char name[128];
+		sprintf(name, "%d-%s", thing_.id(), thing_.name());
+
+		// Set the mesh
+		movableObject_ = O3dSchema::sceneManager->createMovableObject(name, info_.movableObjectType_.get(), &info_.params_);
+		node_->attachObject(movableObject_);
+		particle_ = static_cast<Ogre::ParticleSystem*>(movableObject_);
+	}
+
+
+	ThingParticle
+	::~ThingParticle() {
+		O3dSchema::sceneManager->destroyMovableObject(movableObject_);
+		movableObject_ = 0;
+	}
+
+
+	void ThingParticle
+	::animate(long when, float stepDelta, float timeSinceLastFrame) {
+		float p = thing_.pos().anim(0).pos();
+		float w = thing_.pos().anim(0).weight();
+	}
+
+
+}
