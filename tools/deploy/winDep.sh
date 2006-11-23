36af9fa4649de688326854d02a02890168133ce5
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2006-11-23 11:26:30 +0000

    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@321 65b84acb-c606-4e19-8676-f724b2686c9c

--- tools/deploy/winDep.sh
+++ tools/deploy/winDep.sh
@@ -20,15 +20,19 @@ mkdir include
 mkdir include/ogre
 mkdir include/ogre/samples
 mkdir include/fmod
+mkdir include/ois
 
 cp /c/OgreSDK/lib/* lib/release/
 mv lib/release/*.pdb lib/debug
 mv lib/release/*_d.* lib/debug
 cp lib/release/ode.* lib/debug
+cp ${PROGRAMFILES}/Wreckedgames/OIS_SDK/libs/VC8/OIS_static.lib lib/release
+cp ${PROGRAMFILES}/Wreckedgames/OIS_SDK/libs/VC8/OIS_static_d.lib lib/debug
 cp -R /c/OgreSDK/lib/opt/* lib
 cp /c/Programfiler/FMOD\ SoundSystem/FMOD\ Programmers\ API/api/lib/fmodex_vc.lib lib/release/fmodex.lib
 cp /c/Programfiler/FMOD\ SoundSystem/FMOD\ Programmers\ API/api/lib/fmodex_vc.lib lib/debug/fmodex.lib
 
 cp -R /c/OgreSDK/include/* include/ogre
 cp /c/OgreSDK/samples/include/OgreCEGUI*.h include/ogre/samples
-cp /c/Programfiler/FMOD\ SoundSystem/FMOD\ Programmers\ API/api/inc/* include/fmod/
+cp ${PROGRAMFILES}/FMOD\ SoundSystem/FMOD\ Programmers\ API/api/inc/* include/fmod/
+cp ${PROGRAMFILES}/Wreckedgames/OIS_SDK/includes/* include/ois/
