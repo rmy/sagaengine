4d9f9098d1affc9d7e4bcb173511a1fed5e8acdb
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2007-02-01 21:56:06 +0000

    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@408 65b84acb-c606-4e19-8676-f724b2686c9c

--- env/vc/bootstrap_vc71.sh
+++ env/vc/bootstrap_vc71.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp application_vc71.vctmpl appliaction.vctmpl
+cp application_vc71.vctmpl application.vctmpl
 cp library_vc71.vctmpl library.vctmpl
 
 sh bootstrap.sh
--- env/vc/bootstrap_vc80.sh
+++ env/vc/bootstrap_vc80.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cp application_vc80.vctmpl appliaction.vctmpl
+cp application_vc80.vctmpl application.vctmpl
 cp library_vc80.vctmpl library.vctmpl
 
 sh bootstrap.sh
