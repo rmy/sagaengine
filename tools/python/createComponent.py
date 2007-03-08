e222cb07fde972b50f205ec1be15735948da92b8
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2007-03-08 10:22:29 +0000

    Component template.
    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@445 65b84acb-c606-4e19-8676-f724b2686c9c

---
+++ tools/python/createComponent.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+import os
+import sys
+import string
+
+# Modules are now in the modules.txt file in the script directory
+# Read it from disk
+templates = list();
+DIR = os.path.join(os.path.dirname(sys.argv[0]), ".template")
+for f in os.listdir(DIR):
+	if f.endswith('pp'):
+		path = os.path.join(DIR, f)
+		templates.append(path);
+
+
+
+(namespace, type) = sys.argv[1].split("::")
+namespaceCap = ""
+np = namespace.split("_")
+for p in np:
+	namespaceCap = namespaceCap + p.capitalize()
+		
+
+for t in templates:
+	fin = open(t, "r")
+	filename = "%s%s" % (type, os.path.basename(t))
+	fout = open(filename, "w");
+	for line in fin.readlines():
+		line = line.replace("%s", type)
+		line = line.replace("%S", type.upper())
+		if(namespace == "se_core"):
+			line = line.replace("::%n", "")
+			line = line.replace("using namespace se_core;", "")
+		
+		line = line.replace("%n", namespace)
+		line = line.replace("%N", namespaceCap)
+		fout.write(line)
+		
+	fin.close()
+	fout.close()
+
+
