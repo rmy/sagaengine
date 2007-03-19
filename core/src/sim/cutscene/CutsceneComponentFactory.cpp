836b74f966d723d72ed52adc7f6b100ccef71ccc
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2007-03-19 15:58:30 +0000

    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@476 65b84acb-c606-4e19-8676-f724b2686c9c

---
+++ core/src/sim/cutscene/CutsceneComponentFactory.cpp
@@ -0,0 +1,54 @@
+/*
+SagaEngine library
+Copyright (c) 2002-2006 Skalden Studio AS
+
+This software is provided 'as-is', without any express or implied 
+warranty. In no event will the authors be held liable for any 
+damages arising from the use of this software.
+
+Permission is granted to distribute the library under the terms of the 
+Q Public License version 1.0. Be sure to read and understand the license
+before using the library. It should be included here, or you may read it
+at http://www.trolltech.com/products/qt/licenses/licensing/qpl
+
+The original version of this library can be located at:
+http://www.sagaengine.com/
+
+Rune Myrland
+rune@skalden.com
+*/
+
+
+#include "CutsceneComponentFactory.hpp"
+#include "CutsceneComponent.hpp"
+#include "sim/schema/SimSchema.hpp"
+#include "sim/stat/MultiSimNodeComponent.hpp"
+#include "util/error/Log.hpp"
+
+
+
+
+namespace se_core {
+
+	CutsceneComponentFactory
+	::CutsceneComponentFactory()
+		: SimComponentFactory(sct_CUTSCENE) {
+	}
+
+
+	SimComponent* CutsceneComponentFactory
+	::create(SimComposite* owner) const {
+		// Try to get existing component 
+		// - allows overrides of default values
+		// (Useful when loading saved games).
+		CutsceneComponent::Ptr pCutscene(*owner);
+		if(pCutscene == 0) {
+			// Create new component
+			pCutscene = new CutsceneComponent(owner, this);
+		}
+
+
+		return pCutscene;
+	}
+
+}
---
+++ core/src/sim/cutscene/all.hpp
@@ -0,0 +1,10 @@
+#ifndef sim_cutscene_all_hpp
+#define sim_cutscene_all_hpp
+
+#include "CutsceneAreaComponent.hpp"
+#include "CutsceneAreaComponentFactory.hpp"
+#include "CutsceneComponent.hpp"
+#include "CutsceneComponentFactory.hpp"
+#include "CutsceneManager.hpp"
+
+#endif
