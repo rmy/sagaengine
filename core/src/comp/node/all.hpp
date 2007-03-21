615f2487d674cf09c7a53a430d2be7c386675c86
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2007-03-21 11:48:02 +0000

    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@491 65b84acb-c606-4e19-8676-f724b2686c9c

---
+++ core/src/comp/node/all.hpp
@@ -0,0 +1,9 @@
+#ifndef comp_node_all_hpp
+#define comp_node_all_hpp
+
+#include "AreaChildComponent.hpp"
+#include "NodeComponent.hpp"
+#include "RootChildComponent.hpp"
+#include "RootComponent.hpp"
+
+#endif
