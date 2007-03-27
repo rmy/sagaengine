1615ec58cd873dc9e642dd0bc6d4ac4f6146d3a9
rmy <rmy@65b84acb-c606-4e19-8676-f724b2686c9c>
2007-03-27 23:33:04 +0000

    git-svn-id: http://svn.sagaengine.com/sagaengine/trunk@522 65b84acb-c606-4e19-8676-f724b2686c9c

---
+++ add_on/editor/src/editor/schema/EditorSchema.cpp
@@ -0,0 +1,62 @@
+/*
+SagaEngine library
+Copyright (c) 2002-2006 Skalden Studio AS
+
+This software is provided 'as-is', without any express or implied 
+warranty. In no event will the authors be held liable for any 
+damages arising from the use of this software.
+
+Permission is granted to distribute the library under the terms of the 
+Q Public License version 1.0. Be sure to read and understand the license
+before using the library. It should be included here, or you may read it
+at http://www.trolltech.com/products/qt/licenses/licensing/qpl
+
+The original version of this library can be located at:
+http://www.sagaengine.com/
+
+Rune Myrland
+rune@skalden.com
+*/
+
+
+#include "EditorSchema.hpp"
+#include "editor/comp/EditorManager.hpp"
+#include "sim/schema/SimSchema.hpp"
+#include "util/error/Log.hpp"
+#include "sim/InitListener.hpp"
+#include "sim/InitListeners.hpp"
+
+using namespace se_core;
+
+namespace se_editor {
+	namespace EditorSchema {
+		class _SeBasicExport AutoInit : public se_core::InitListener {
+		public:
+			AutoInit() {
+
+				// Register as init event listener
+				SimSchema::initListeners().addListener(*this);
+				LogDetail("Registered Editor add-on");
+			}
+
+			~AutoInit() {
+				SimSchema::initListeners().removeListener(*this);
+				LogDetail("Cleaned up Editor add-on");
+			}
+
+			bool initEngineEvent() {
+				EditorManager::singleton();
+				return true; 
+			}
+			void cleanupEngineEvent() {}
+
+			bool initGameEvent() { return true;}
+			void cleanupGameEvent() {}
+
+			bool initLevelEvent() { return true; }
+			void cleanupLevelEvent() {}
+
+		} autoInit;
+	}
+
+}
